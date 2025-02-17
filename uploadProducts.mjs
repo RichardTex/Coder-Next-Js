be4c794cf16cf4336358626af77dfd8cb313edd9
Mariano <60629936+RichardTex@users.noreply.github.com>
2025-02-17 11:23:53 -0300

    proyecto final ultimos cambios

---
+++ uploadProducts.mjs
@@ -0,0 +1,37 @@
+import { collection, doc, setDoc } from "firebase/firestore";
+import { db } from "./utils/config.js";
+import { readFileSync, existsSync, writeFileSync } from "node:fs";
+
+const filePath = 'productos.json';
+if (!existsSync(filePath)) {
+    console.error(`El archivo no existe en la ruta: ${filePath}`);
+    process.exit(1);
+}
+
+const products = JSON.parse(readFileSync(filePath, "utf8"));
+
+const productsToUpload = products.filter(product => !product.isDeleted);
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const uploadProducts = async () => {
+    try {
+        const productsCollection = collection(db, "productos");
+
+        for (const product of products) {            
+
+            const docRef = doc(productsCollection, product.id);
+
+            await setDoc(docRef, product);            
+
+            await delay(200);
+        }
+
+        writeFileSync(filePath, JSON.stringify(productsToUpload, null, 2), "utf8");
+
+    } catch (error) {
+        console.error("Error uploading or updating products:", error);
+    }
+};
+
+uploadProducts();
