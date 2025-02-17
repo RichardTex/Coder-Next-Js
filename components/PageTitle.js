27e306a4f895bbb83757e664ee7336ddac9b3832
Mariano <60629936+RichardTex@users.noreply.github.com>
2025-02-17 11:17:51 -0300

    Delete components directory

--- components/PageTitle.js
+++
@@ -1,9 +0,0 @@
-function PageTitle({ children }) {
-    return (
-        <h1 className="text-3xl font-bold text-gray-800 mb-6">
-            {children}
-        </h1>
-    );
-}
-
-export default PageTitle;
